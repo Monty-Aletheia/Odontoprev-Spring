4d21e63a1eabbfe9b771a30d699ddf8913eac359
Pedro Jesus <pedromra.04@gmail.com>
2024-11-05 17:12:36 -0300

    fix: fixed migration

--- src/main/resources/db/migration/V3__update_dentist.sql
+++ src/main/resources/db/migration/V3__update_dentist.sql
@@ -1,3 +1,9 @@
 ALTER TABLE tb_dentist
-    ADD password VARCHAR2(255) DEFAULT 'default_password' NOT NULL;
+    ADD password VARCHAR2(255);
 
+UPDATE tb_dentist
+SET password = '1234'
+WHERE password IS NULL;
+
+ALTER TABLE tb_dentist
+    MODIFY password VARCHAR2(255) NOT NULL;
