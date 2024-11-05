30a55d89a3c97fde7379e069a7e73a795a1c8317
Pedro Jesus <pedromra.04@gmail.com>
2024-11-05 17:15:55 -0300

    feat: Implement AuthController with signIn and signUp endpoints
    - Created AuthController class with two endpoints:
      - POST /auth/signIn: Placeholder for dentist login functionality.
      - POST /auth/signup: Placeholder for dentist registration functionality.

--- src/main/java/com/fiap/br/challenger/application/controller/AuthController.java
+++ src/main/java/com/fiap/br/challenger/application/controller/AuthController.java
@@ -1,9 +1,24 @@
 package com.fiap.br.challenger.application.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+
+    @PostMapping("/signIn")
+    public ResponseEntity<?> SignIn(){
+        // TODO fazer rota de login do dentista
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> SignUp(){
+        // TODO fazer rota de cadastro do dentista
+        return ResponseEntity.ok().build();
+    }
 }
