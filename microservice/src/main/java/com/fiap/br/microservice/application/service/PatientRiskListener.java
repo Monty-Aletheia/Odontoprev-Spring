14a715da4257926464950af625ecbe51078ed45c
Pedro Jesus <Pedromra.04@gmail.com>
2025-04-18 08:42:06 -0300

    feat: implement core functionality of patient risk microservice

--- microservice/src/main/java/com/fiap/br/microservice/application/service/PatientRiskListener.java
+++ microservice/src/main/java/com/fiap/br/microservice/application/service/PatientRiskListener.java
@@ -1,21 +1,28 @@
 package com.fiap.br.microservice.application.service;
 
-import com.fiap.br.microservice.application.repository.PatientExternalRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PatientRiskListener {
 
-    private final PatientExternalRepository patientExternalRepository;
+    private final MessageService messageService;
 
-    public PatientRiskListener(PatientExternalRepository patientExternalRepository) {
-        this.patientExternalRepository = patientExternalRepository;
+    public PatientRiskListener(MessageService messageService) {
+        this.messageService = messageService;
     }
 
     @RabbitListener(queues = "patient-risk-queue")
-    public void receberMensagem(String mensagem) {
-        System.out.println("Mensagem recebida: " + mensagem);
+    public void receiveMessage(String messageJson) {
+        ObjectMapper mapper = new ObjectMapper();
 
+        try {
+            JsonNode root = mapper.readTree(messageJson);
+            messageService.predictPatientRisk(root);
+        } catch (Exception e) {
+            System.err.println("Erro ao processar a mensagem JSON: " + e.getMessage());
+        }
     }
 }
