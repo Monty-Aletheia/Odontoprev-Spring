ef4e858feab3b6e62868ae3094e8bfcc8acbc11e
Pedro Jesus <Pedromra.04@gmail.com>
2025-04-18 08:39:45 -0300

    fix: sanitize some json fields in PatientRiskMessageBuilder

--- Aletheia-monolito/src/main/java/com/fiap/br/challenger/application/service/mq/PatientRiskMessageBuilder.java
+++ Aletheia-monolito/src/main/java/com/fiap/br/challenger/application/service/mq/PatientRiskMessageBuilder.java
@@ -9,30 +9,30 @@ import java.util.UUID;
 @Component
 public class PatientRiskMessageBuilder {
 
-    public JSONObject build(UUID pacienteId, PatientRiskAssessmentDTO dto) {
+    public JSONObject build(UUID patientId, PatientRiskAssessmentDTO dto) {
         JSONObject json = new JSONObject();
-        json.put("pacient_id", pacienteId);
+        json.put("patient_id", patientId);
 
-        JSONObject risco = new JSONObject();
-        json.put("idade", dto.age());
-        json.put("genero", dto.gender());
-        json.put("frequencia_consultas", dto.consultationFrequency());
-        json.put("aderencia_tratamento", 0);
-        json.put("historico_caries", dto.cavitiesHistory());
-        json.put("doenca_periodontal", dto.periodontalDisease());
-        json.put("numero_implantes", dto.numberOfImplants());
-        json.put("fumante", dto.smoker());
-        json.put("alcoolismo", dto.alcoholism());
-        json.put("escovacao_diaria", dto.dailyBrushing());
-        json.put("uso_fio_dental", dto.flossing());
-        json.put("doencas_sistemicas", dto.systemicDiseases().getDescricao());
-        json.put("medicamentos_uso_continuo", dto.continuousMedicationUse());
-        json.put("numero_sinistros_previos", 0);
-        json.put("valor_medio_sinistros", 0);
-        json.put("tratamentos_complexos_previos", dto.previousComplexTreatments());
-        json.put("tipo_plano", dto.planType().getDescricao());
+        JSONObject risk = new JSONObject();
+        risk.put("idade", dto.age());
+        risk.put("genero", dto.gender());
+        risk.put("frequencia_consultas", dto.consultationFrequency());
+        risk.put("aderencia_tratamento", 0);
+        risk.put("historico_caries", dto.cavitiesHistory());
+        risk.put("doenca_periodontal", dto.periodontalDisease());
+        risk.put("numero_implantes", dto.numberOfImplants());
+        risk.put("fumante", dto.smoker());
+        risk.put("alcoolismo", dto.alcoholism());
+        risk.put("escovacao_diaria", dto.dailyBrushing());
+        risk.put("uso_fio_dental", dto.flossing());
+        risk.put("doencas_sistemicas", dto.systemicDiseases().getDescricao());
+        risk.put("medicamentos_uso_continuo", dto.continuousMedicationUse());
+        risk.put("numero_sinistros_previos", 0);
+        risk.put("valor_medio_sinistros", 0);
+        risk.put("tratamentos_complexos_previos", dto.previousComplexTreatments());
+        risk.put("tipo_plano", dto.planType().getDescricao());
 
-        json.put("risk", risco);
+        json.put("risk", risk);
         return json;
     }
 }
